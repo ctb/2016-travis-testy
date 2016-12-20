5cab5a0042a2fdb219bf5f6f3243a2cabca6ad09
C. Titus Brown <titus@idyll.org>
2016-12-20 14:48:25 -0800

    swap screed/khmer

--- update-software.sh
+++ update-software.sh
@@ -5,5 +5,5 @@ set -e
 
 source ~/pondenv/bin/activate
 
-pip install git+https://github.com/dib-lab/khmer.git@master
 pip install git+https://github.com/dib-lab/screed.git@master
+pip install git+https://github.com/dib-lab/khmer.git@master
